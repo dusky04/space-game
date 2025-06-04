3c566a403ff154d7e68450bacb2297a0479e7541
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2025-06-04 19:17:44 +0000

    Major Refactor

--- spaceship.c
+++ spaceship.c
@@ -1,32 +1,9 @@
-#include "raylib.h"
-#include "raymath.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 750
-
-#define SPACESHIP_SPEED 400
-
-#define LASER_LENGTH 50
-#define LASER_SPEED 600
-
-#define NUM_STARS 30
-#define NUM_LASERS 10
-
-#define BACKGROUND_COLOR                                                       \
-  CLITERAL(Color) { 28, 22, 37, 255 }
-
-#define RANDINT(min, max) (rand() % (max - min + 1) + min)
-#define RANDFLOAT(min, max) (((max - min) * (float)rand() / RAND_MAX) + min)
-
-typedef struct {
-  Texture2D texture;
-  Vector2 position;
-  Vector2 direction;
-} Spaceship;
+#include "spaceship.h"
 
 Spaceship CreateSpaceship(const char *filename) {
   Spaceship spaceship = {0};
@@ -37,17 +14,24 @@ Spaceship CreateSpaceship(const char *filename) {
   return spaceship;
 }
 
-typedef struct {
-  Texture2D texture;
-  Vector2 position;
-  float scale;
-} Star;
+void UpdateSpaceshipPosition(Spaceship *spaceship, float dt) {
+  spaceship->position.x += dt * SPACESHIP_SPEED * spaceship->direction.x;
+  spaceship->position.x =
+      Clamp(spaceship->position.x, 0, SCREEN_WIDTH - spaceship->texture.width);
+  spaceship->position.y += dt * SPACESHIP_SPEED * spaceship->direction.y;
+  spaceship->position.y = Clamp(spaceship->position.y, 0,
+                                SCREEN_HEIGHT - spaceship->texture.height);
+}
 
-Star *CreateStars(const char *filename) {
+void DeleteSpaceship(Spaceship *spaceship) {
+  UnloadTexture(spaceship->texture);
+}
+
+Star *CreateStars(Texture2D *texture) {
   Star *stars = (Star *)malloc(sizeof(Star) * NUM_STARS);
   for (size_t i = 0; i < NUM_STARS; i++) {
     // TODO: perhaps load the texture only once since its the same asset?
-    stars[i].texture = LoadTexture(filename);
+    stars[i].texture = texture;
     stars[i].position = (Vector2){.x = RANDINT(0, SCREEN_WIDTH),
                                   .y = RANDINT(0, SCREEN_HEIGHT)};
     stars[i].scale = RANDFLOAT(0.5, 1.6);
@@ -57,31 +41,35 @@ Star *CreateStars(const char *filename) {
 
 void DeleteStars(Star *stars) {
   for (size_t i = 0; i < NUM_STARS; i++) {
-    UnloadTexture(stars[i].texture);
+    UnloadTexture(*stars[i].texture);
   }
   free(stars);
 }
 
-typedef struct {
-  Texture2D texture;
-  Vector2 position;
-  bool inview;
-} Laser;
-
-Laser *CreateLaser(const char *filename) {
+Laser *CreateLaser(Texture2D *texture) {
   Laser *lasers = (Laser *)malloc(sizeof(Laser) * NUM_LASERS);
-  for (size_t i = 0; i < NUM_STARS; i++) {
+  for (size_t i = 0; i < NUM_LASERS; i++) {
     // TODO: perhaps load the texture only once since its the same asset?
-    lasers[i].texture = LoadTexture(filename);
+    lasers[i].texture = texture;
     lasers[i].position = (Vector2){0};
     lasers[i].inview = false;
   }
   return lasers;
 }
 
+void UpdateLasers(Laser *lasers, float dt) {
+  for (size_t i = 0; i < NUM_LASERS; i++) {
+    if (lasers[i].inview)
+      lasers[i].position.y -= dt * LASER_SPEED;
+
+    if (lasers[i].position.y < 0)
+      lasers[i].inview = false;
+  }
+}
+
 void DeleteLasers(Laser *lasers) {
   for (size_t i = 0; i < NUM_LASERS; i++) {
-    UnloadTexture(lasers[i].texture);
+    UnloadTexture(*lasers[i].texture);
   }
   free(lasers);
 }
@@ -95,43 +83,35 @@ int main() {
 
   // load the spaceship texture
   Spaceship spaceship = CreateSpaceship("assets/spaceship.png");
-  Star *stars = CreateStars("assets/star.png");
-  Laser *lasers = CreateLaser("assets/laser.png");
+
+  Texture2D star_texture = LoadTexture("assets/star.png");
+  Texture2D laser_texture = LoadTexture("assets/laser.png");
+
+  Star *stars = CreateStars(&star_texture);
+  Laser *lasers = CreateLaser(&laser_texture);
 
   int laser_idx = 0;
 
   while (!WindowShouldClose()) {
     //  -----------------------------------------------------------------------
-    // updating the frame
+    // handling user input
     spaceship.direction.x = IsKeyDown(KEY_D) - IsKeyDown(KEY_A);
     spaceship.direction.y = IsKeyDown(KEY_S) - IsKeyDown(KEY_W);
     spaceship.direction = Vector2Normalize(spaceship.direction);
 
-    if (IsKeyPressed(KEY_SPACE)) {
+    if (IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
       laser_idx = (laser_idx + 1) % NUM_LASERS;
       lasers[laser_idx].inview = true;
       lasers[laser_idx].position =
-          (Vector2){.x = spaceship.position.x, .y = spaceship.position.y};
+          (Vector2){.x = spaceship.position.x + spaceship.texture.width / 2.2,
+                    .y = spaceship.position.y - spaceship.texture.height / 2.0};
     }
 
-    // TODO: refactor it into a method
-    // update player position
+    //  -----------------------------------------------------------------------
+    // updating the frame
     float dt = GetFrameTime(); // delta time
-    spaceship.position.x += dt * SPACESHIP_SPEED * spaceship.direction.x;
-    spaceship.position.x =
-        Clamp(spaceship.position.x, 0, SCREEN_WIDTH - spaceship.texture.width);
-    spaceship.position.y += dt * SPACESHIP_SPEED * spaceship.direction.y;
-    spaceship.position.y = Clamp(spaceship.position.y, 0,
-                                 SCREEN_HEIGHT - spaceship.texture.height);
-
-    // update laser
-    for (size_t i = 0; i < NUM_LASERS; i++) {
-      if (lasers[i].inview)
-        lasers[i].position.y -= dt * LASER_SPEED;
-
-      if (lasers[i].position.y < 0)
-        lasers[i].inview = false;
-    }
+    UpdateSpaceshipPosition(&spaceship, dt);
+    UpdateLasers(lasers, dt);
 
     //  -----------------------------------------------------------------------
     // handle drawing on the screen
@@ -139,12 +119,12 @@ int main() {
     {
       ClearBackground(BACKGROUND_COLOR);
       for (size_t i = 0; i < NUM_STARS; i++)
-        DrawTextureEx(stars[i].texture, stars[i].position, 0.0, stars[i].scale,
+        DrawTextureEx(*stars[i].texture, stars[i].position, 0.0, stars[i].scale,
                       WHITE);
       DrawTextureV(spaceship.texture, spaceship.position, WHITE);
       for (size_t i = 0; i < NUM_LASERS; i++) {
         if (lasers[i].inview)
-          DrawTextureV(lasers[i].texture, lasers[i].position, WHITE);
+          DrawTextureV(*lasers[i].texture, lasers[i].position, WHITE);
       }
       DrawFPS(0, 0);
     }
@@ -153,11 +133,8 @@ int main() {
 
   // ----------------------------------------------------------------------
   // cleanup
-  UnloadTexture(spaceship.texture);
+  DeleteSpaceship(&spaceship);
   DeleteLasers(lasers);
   DeleteStars(stars);
   CloseWindow();
 }
-
-// when i press the left mouse button
-// i should fire a laser
---
+++ spaceship.h
@@ -0,0 +1,59 @@
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+#include "raylib.h"
+#include "raymath.h"
+
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 750
+
+#define SPACESHIP_SPEED 400
+#define LASER_SPEED 600
+#define ASTEROID_SPEED 400
+
+#define NUM_STARS 30
+#define NUM_LASERS 10
+#define NUM_ASTEROIDS 10
+
+#define BACKGROUND_COLOR                                                       \
+  CLITERAL(Color) { 28, 22, 37, 255 }
+
+#define RANDINT(min, max) (rand() % (max - min + 1) + min)
+#define RANDFLOAT(min, max) (((max - min) * (float)rand() / RAND_MAX) + min)
+
+typedef struct {
+  Texture2D texture;
+  Vector2 position;
+  Vector2 direction;
+} Spaceship;
+
+Spaceship CreateSpaceship(const char *filename);
+void UpdateSpaceshipPosition(Spaceship *spaceship, float dt);
+void DeleteSpaceship(Spaceship *spaceship);
+
+typedef struct {
+  Texture2D *texture;
+  Vector2 position;
+  float scale;
+} Star;
+
+Star *CreateStars(Texture2D *texture);
+void DeleteStars(Star *stars);
+
+typedef struct {
+  Texture2D *texture;
+  Vector2 position;
+  bool inview;
+} Laser;
+
+Laser *CreateLaser(Texture2D *texture);
+void UpdateLasers(Laser *lasers, float dt);
+void DeleteLasers(Laser *lasers);
+
+typedef struct {
+  Texture2D texture;
+  Vector2 position;
+  Vector2 direction;
+} Meteor;
+
+#endif
